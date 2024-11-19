84ca54cb2af18dd996880485af73ab114ef4fe26
BÁRBARA ROSA SABINO <barbara_sabino@estudante.sesisenai.org.br>
2024-11-19 10:47:04 -0300

    ro

--- server/db.js
+++ server/db.js
@@ -43,86 +43,90 @@ const User = sequelize.define('user', {
     }
 })
 
-const Artista = sequelize.define('Artist', {
+const Artista = sequelize.define('artista', {
     nome: {
-        type: Sequelize.DataTypes.STRING,
+        type:Sequelize.DataTypes.STRING,
         allowNull: false,
     },
     bio: {
-        type: Sequelize.DataTypes.TEXT,
+        type:Sequelize.DataTypes.TEXT,
         allowNull: true,
     },
     imageUrl: {
-        type: Sequelize.DataTypes.STRING,
+        type:Sequelize.DataTypes.STRING,
         allowNull: true,
     }
-})
+}, {
+    tableName: 'artists',
+});
 
-
-const Album = sequelize.define('Album', {
+const Album = sequelize.define('album', {
     title: {
-        type: Sequelize.DataTypes.STRING,
+        type:Sequelize.DataTypes.STRING,
         allowNull: false,
     },
     releaseYear: {
-        type: Sequelize.DataTypes.INTEGER,
+        type:Sequelize.DataTypes.INTEGER,
         allowNull: false,
     },
     coverImageUrl: {
-        type: Sequelize.DataTypes.STRING,
+        type:Sequelize.DataTypes.STRING,
         allowNull: true,
     },
+}, {
+    tableName: 'albums',
 });
 
-Album.belongsTo(Artista, {
-    foreignKey: 'artistaId',
-    onDelete: 'CASCADE',
-});
-
-Artista.hasMany(Album, {
-    foreignKey: 'artistaId',
-    as: 'Albums'
-  });
-
-const Musica = sequelize.define('Musica', {
+const Musica = sequelize.define('musica', {
     titulo: {
-        type: Sequelize.DataTypes.STRING,
+        type:Sequelize.DataTypes.STRING,
         allowNull: false,
     },
     duracao: {
-        type: Sequelize.DataTypes.INTEGER,  // Duração em segundos
+        type:Sequelize.DataTypes.INTEGER,
         allowNull: false,
     },
     fileUrl: {
-        type: Sequelize.DataTypes.STRING,
+        type:Sequelize.DataTypes.STRING,
         allowNull: false,
     },
+}, {
+    tableName: 'musicas',
 });
 
-Musica.belongsTo(Album, {
-    foreignKey: 'albumId',
-    onDelete: 'CASCADE',
-});
-Musica.belongsTo(Artista, {
-    foreignKey: 'artistaId',
+Album.belongsTo(Artista, {
+    foreignKey: 'artista_id',
     onDelete: 'CASCADE',
 });
 Album.hasMany(Musica, {
-    foreignKey: 'albumId',
-    as: 'Musicas'
-  });
+    foreignKey: 'album_id',
+    as: 'Musicas',
+});
+Artista.hasMany(Album, {
+    foreignKey: 'artista_id',
+    as: 'Albums',
+});
+Musica.belongsTo(Album, {
+    foreignKey: 'album_id',
+    onDelete: 'CASCADE',
+});
+Musica.belongsTo(Artista, {
+    foreignKey: 'artista_id',
+    onDelete: 'CASCADE',
+});
 
-  const criarTabelas = () => {
+const criarTabelas = () => {
     sequelize.authenticate().then(() => {
         console.log('conectou')
     })
         .catch((err) => {
             console.log(err)
         })
-    sequelize.sync({ alter: true }).then(() => {
+    sequelize.sync({ force: true }).then(() => {
         console.log('tabela criada')
     })
 }
 
-
 export { User, sequelize, criarTabelas, Artista, Album, Musica };
+
+
--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import Express from "express";
-//import { criarTabelas } from "./db.js";
+import { criarTabelas } from "./db.js";
 import cors from "cors";
 import {rotas} from './roteamento/rotas_autenticacao.js';
 import {rota} from  './roteamento/rota_usuarios.js'
+import { rotas_artistas } from './roteamento/rota_artista.js'
+import { rotas_albums } from "./roteamento/rota_album.js";
 
 const App = Express()
 App.use(Express.json())
@@ -11,5 +13,7 @@ App.use(cors())
 
 App.use('/autenticacao', rotas)
 App.use('/usuarios', rota)
+App.use('/artista', rotas_artistas)
+App.use('/album', rotas_albums)
 
 App.listen(8000)
